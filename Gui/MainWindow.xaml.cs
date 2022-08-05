5374e77379f3f4f137e9cf9e1a36a8156d97dcf3
hagen <hagenhuebner02@yahoo.com>
2022-08-06 00:19:27 +0200

    GUI: Add Start / Stop button and filter for file size.

--- Gui/MainWindow.xaml.cs
+++ Gui/MainWindow.xaml.cs
@@ -12,11 +12,13 @@ namespace Gui
         {
             InitializeComponent();
             UpdateButtonState();
+            SizeUnitSelection.SelectedIndex = 1;
         }
 
         private void UpdateButtonState() 
         {
             RemoveButton.IsEnabled = FoldersToSearchList.SelectedIndex != -1;
+            StopButton.IsEnabled = !StartButton.IsEnabled;
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
@@ -28,8 +30,12 @@ namespace Gui
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
-            dialog.Multiselect = true;
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog
+            {
+                Multiselect = true,
+                Description = "Add directories to search.",
+                UseDescriptionForTitle = true
+            };
             if (dialog.ShowDialog(this).GetValueOrDefault())
             {
                 foreach (var p in dialog.SelectedPaths)
@@ -40,6 +46,16 @@ namespace Gui
         private void FoldersToSearchList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateButtonState();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
