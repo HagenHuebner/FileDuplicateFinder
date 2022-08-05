34584425709a0b2715a9ad447dde5c82ced95f76
hagen <hagenhuebner02@yahoo.com>
2022-08-05 21:21:06 +0200

    Add GUI with basic file selection.

---
+++ Gui/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateButtonState();
+        }
+
+        private void UpdateButtonState() 
+        {
+            RemoveButton.IsEnabled = FoldersToSearchList.SelectedIndex != -1;
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var si = FoldersToSearchList.SelectedIndex;
+            if(si != -1)
+                FoldersToSearchList.Items.RemoveAt(si);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                foreach (var p in dialog.SelectedPaths)
+                    FoldersToSearchList.Items.Add(p);
+            }
+        }
+
+        private void FoldersToSearchList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateButtonState();
+        }
+    }
+}
